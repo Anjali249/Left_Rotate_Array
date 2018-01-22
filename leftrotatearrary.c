b7f03b181dabf79aec37081d48176957445ef7ff
Anjali <34779830+Anjali249@users.noreply.github.com>
2018-01-22 11:14:24 -0800

    Update leftrotatearrary.c

--- leftrotatearrary.c
+++ leftrotatearrary.c
@@ -1,33 +1,28 @@
-#include <math.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <limits.h>
-#include <stdbool.h>
+//C Program to left rotate an arry by n positions
 
-void leftrotateone(int arr[], int n)
-{
+#include <stdio.h>
+#include <stdlib.h>
+
+//Function to left shift array by 1 position
+void leftrotateone(int arr[], int n){
     int z,i,j;
     z= arr[0];
-    for(i=0;i<n-1;i++)
-    {
+    for(i=0;i<n-1;i++){
         arr[i]=arr[i+1];
     }
         arr[i]=z;  
 }
 
-void leftrotate(int arr[], int n, int k)
-{
+//Left shift array by k positions
+void leftrotate(int arr[], int n, int k){
     int i,j;
-    for(i=0; i<k; i++)
-    {
+    for(i=0; i<k; i++){
         leftrotateone(arr, n);
     }
 }
 
-void printarray(int arr[], int n)
-{
+//Function to Print Array
+void printarray(int arr[], int n){
     int x;
     for(x=0;x<n;x++)
     {
@@ -38,25 +33,20 @@ void printarray(int arr[], int n)
 
 int main()
 {
-    int n; 
-    int k,a_i,i,j,x, z=0; 
+    int k,a_i,n; 
     printf("Enter size of array: ");
     scanf("%d",&n);
     printf("Enter number of rotation: ");
     scanf("%d",&k);
-
+    
     int *a = malloc(sizeof(int) * n);
 
-    for(a_i = 0; a_i < n; a_i++)
-    {
+    for(a_i = 0; a_i < n; a_i++){
        printf("Enter element %d : ",a_i+1);
        scanf("%d",&a[a_i]);
-    }
-
-    
+    } 
     leftrotate(a,n,k);
     printf("Left shifted array by %d rotation is: ",k);
     printarray(a, n);
-    
     return 0;
 }
